62906e0b71c957f69e5e53bd86ffada56a88ebff
Flip <flipsyde59@mail.ru>
2020-06-02 14:52:55 +0500

    Primal

--- source.py
+++ source.py
@@ -4,10 +4,9 @@ class Test(mglw.WindowConfig):
     gl_version = (3, 3)
     window_size = (200, 200)
     def render(self, time, frametime):
-        self.ctx.clear(0.0, 1.0, 1.0, 0.0)
+        self.ctx.clear(0.0, 0.0, 1.0, 0.0)
 
 
 
 mglw.run_window_config(Test)
-
 
