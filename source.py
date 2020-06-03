3491794fd709c8e9298e4a5e7b0fdbb3603f0654
Flip <flipsyde59@mail.ru>
2020-06-03 18:24:24 +0500

    horizontal lines

--- source.py
+++ source.py
@@ -13,33 +13,41 @@ class Test(mglw.WindowConfig):
         self.prog = self.ctx.program(
             vertex_shader='''
                 #version 330
-                in vec2 a_position;
-                in float a_t;
-                out float v_t;
-                void main() {
-                    v_t=a_t
+
+                layout(location = 0) in vec2 a_position;
+                layout(location = 1) in vec3 a_color;
+
+                out vec3 v_color;
+
+                void main()
+                {
                     gl_Position = vec4(a_position, 0.0, 1.0);
+                    v_color = a_color;
                 }
-            ''',
+                ''',
             fragment_shader='''
                 #version 330
-                in float v_t;
-                out vec4 o_color;
-                void main() {
-                    o_color = vec4(sin(v_t*62.83), 0.0, 0.0, 1.0);
+
+                in vec3 v_color;
+                out vec4 out_color;
+                
+                void main()
+                {
+                    float r = v_color.r;
+                    out_color = vec4(sin(r * 62.83), 0.0, 0.0, 0.0);
                 }
-            ''',
+                ''',
         )
 
         vertices = np.array([
-            0.5, 0.5,
-            -0.5, -0.5,
-            0.5, -0.5,
-            -0.5, 0.5
+            0.5, 0.5, 1.0, 0.0, 0.0,
+            -0.5, 0.5, 1.0, 0.0, 0.0,
+            -0.5, -0.5, 0.0, 0.0, 0.0,
+            0.5, -0.5, 0.0, 0.0, 0.0,
         ])
 
-        self.vbo = self.ctx.buffer(vertices.astype('f4').tobytes())
-        self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'in_vert', 'in_color')
+        self.vbo = self.ctx.buffer(vertices.astype('f4').tobytes()*4)
+        self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'a_position', 'a_color')
 
     def render(self, time, frametime):
         self.ctx.clear(0.0, 0.0, 0.0, 1.0)
